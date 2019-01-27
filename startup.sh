1e4ae1cb52874958f7e13f3bfddb308b4f690e1e
Einsten42 <milne.james@gmail.com>
2019-01-27 15:38:56 -0600

    inital release

--- startup.sh
+++ startup.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 [[ -z "$PGURL" ]] && { echo "PGURL environment variable not found. Exiting."; exit 1; }
+
 curl -X GET $PGURL -o .env
-
+cat .env
 GITURL=`cat .env | jq '.nodeServer.url' | tr -d '"'`
+MQTTURL=`cat .env | jq '.mqttUrl' | tr -d '"'`
 [[ -z "$GITURL" ]] && { echo "GIT URL not found in config. Exiting."; exit 1; }
+[[ -z "$MQTTURL" ]] && { echo "MQTT URL not found in config. Exiting."; exit 1; }
+NODESERVER=`cat .env | jq '.nodeServer'`
 
-git clone --depth 1 $GITURL nodeserver
-cd "${0%/*}"/nodeserver
+if [ -z "$LOCAL" ]
+then
+  git clone --depth 1 $GITURL nodeserver
+else
+  ln -s /app/template/ nodeserver
+  ln -s /app/pgc_interface nodeserver/pgc_interface
+fi
+
+cd /app/nodeserver
+head -5 /app/nodeserver/pgc_interface/pgc_interface.py
+
 
 CLOUDINSTALL=`cat server.json | jq '.install_cloud' | tr -d '"'`
 [[ -z "$CLOUDINSTALL" ]] && { echo ".install_cloud not found in server.json. Exiting."; exit 1; }
@@ -17,8 +30,15 @@ EXECUTABLE=`cat server.json | jq '.executable' | tr -d '"'`
 [[ -z "$EXECUTABLE" ]] && { echo "executable not found in server.json. Exiting."; exit 1; }
 
 # [[ $TYPE == "python" ]] && { /usr/bin/env pip install pgc_interface; }
-# [[ $TYPE == "python3" ]] && { /usr/bin/env pip3 install pgc_interface; }
-# [[ $TYPE == "node" ]] && { /usr/bin/env npm install; }
+if [ $TYPE == "python3" ]
+then
+  if [ -z "$LOCAL" ]
+  then
+    /usr/bin/env pip3 install pgc_interface
+  fi
+  # /usr/bin/env pip3 install paho-mqtt
+fi
+# [[ $TYPE == "node" ]] && { /usr/bin/env npm install pgc_interface; }
 
 /usr/bin/env bash -xe ./$CLOUDINSTALL
-/usr/bin/env $TYPE ./$EXECUTABLE
+PGURL=$PGURL MQTTURL=$MQTTURL NODESERVER=$NODESERVER /usr/bin/env $TYPE ./$EXECUTABLE
